6c6aa8d35000d53a15aa1d72c167fee74340275a
Emilio Rojas <emilio93@gmail.com>
2019-05-14 16:28:19 -0600

    Update main to turn on led using driverlib

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "msp.h"
-
+#include "driverlib.h"
 
 /**
  * main.c
@@ -7,4 +7,8 @@
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+	MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
+	while (1) {}
 }
