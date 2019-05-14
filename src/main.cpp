9a0ae480a699ea1d0df13498ae6aeb43eb9c4736
Emilio Rojas <emilio93@gmail.com>
2019-05-14 16:37:19 -0600

    Apply format

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "msp.h"
+
 #include "driverlib.h"
 
 /**
  * main.c
  */
-void main(void)
-{
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+void main(void) {
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;  // stop watchdog timer
 
 	MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
 	MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
-	while (1) {}
+	while (1) {
+	}
 }
