6eacc7d73ffd848c740944e6f9d272c814c358d2
Emilio Rojas <emilio93@gmail.com>
2019-05-15 05:38:33 -0600

    Start screen with value in accelerometer

--- src/task/Accelerometer.cpp
+++ src/task/Accelerometer.cpp
@@ -35,7 +35,7 @@ uint8_t task::Accelerometer::setup() {
 
 	GrContextFontSet(&task::Accelerometer::m_stStaticContext, &g_sFontFixed6x8);
 
-	this->drawBackground();
+	this->updateBackground();
 	this->printResults();
 
 	this->getAccelerometer()->TriggerConversion();
@@ -178,119 +178,6 @@ void task::Accelerometer::updateBackground() {
 		               l_u8YStart + l_u8YSeparation * i);
 		l_isToggle = !l_isToggle;
 	}
-}
-
-void task::Accelerometer::drawBackground() {
-	int i;
-	uint8_t j, k;
-	uint8_t l_u8LongLineHalfLenght = 8;
-	uint8_t l_u8ShortLineHalfLenght = 4;
-	uint8_t l_u8XCenter = 64;
-	uint8_t l_u8YStart = 23;
-	uint8_t l_u8YSeparation = 10;
-	bool l_isToggle = true;
-
-	Graphics_clearDisplay(&task::Accelerometer::m_stStaticContext);
-
-	Graphics_setForegroundColor(&task::Accelerometer::m_stStaticContext,
-	                            GRAPHICS_COLOR_LIGHT_BLUE);
-	Graphics_Rectangle l_stRectangleBlue = {0, 0, 127, 63};
-	Graphics_fillRectangle(&task::Accelerometer::m_stStaticContext,
-	                       &l_stRectangleBlue);
-
-	Graphics_setForegroundColor(&task::Accelerometer::m_stStaticContext,
-	                            GRAPHICS_COLOR_WHITE);
-	Graphics_drawCircle(&task::Accelerometer::m_stStaticContext, 64, 64, 56);
-
-	Graphics_setForegroundColor(&task::Accelerometer::m_stStaticContext,
-	                            GRAPHICS_COLOR_LIGHT_BLUE);
-	Graphics_Rectangle l_stRectangleBlueRefill = {0, 38, 127, 63};
-	Graphics_fillRectangle(&task::Accelerometer::m_stStaticContext,
-	                       &l_stRectangleBlueRefill);
-
-	Graphics_setForegroundColor(&task::Accelerometer::m_stStaticContext,
-	                            GRAPHICS_COLOR_PERU);
-	Graphics_Rectangle l_stRectangleBrown = {0, 64, 127, 127};
-	Graphics_fillRectangle(&task::Accelerometer::m_stStaticContext,
-	                       &l_stRectangleBrown);
-
-	Graphics_setForegroundColor(&task::Accelerometer::m_stStaticContext,
-	                            GRAPHICS_COLOR_WHITE);
-
-	this->drawLine(0, 63, 127, 63);
-	this->drawLine(0, 64, 127, 64);
-
-	this->drawLine(63, 8, 63, 2);
-	this->drawLine(64, 8, 64, 2);
-
-	l_u8LongLineHalfLenght = 8;
-	l_u8ShortLineHalfLenght = 4;
-	l_u8XCenter = 64;
-	l_u8YStart = 23;
-	l_u8YSeparation = 10;
-	l_isToggle = true;
-
-	for (i = 0; i < 4; i++) {
-		j = l_isToggle ? l_u8XCenter - l_u8LongLineHalfLenght
-		               : l_u8XCenter - l_u8ShortLineHalfLenght;
-		k = l_isToggle ? l_u8XCenter + l_u8LongLineHalfLenght
-		               : l_u8XCenter + l_u8ShortLineHalfLenght;
-
-		this->drawLine(j, l_u8YStart + l_u8YSeparation * i, k,
-		               l_u8YStart + l_u8YSeparation * i);
-		l_isToggle = !l_isToggle;
-	}
-
-	l_u8YStart = 10;
-	for (i = 0; i < 5; i++) {
-		this->drawLine(l_u8XCenter - i, l_u8YStart + i, l_u8XCenter + i,
-		               l_u8YStart + i);
-	}
-
-	l_u8YStart = 74;
-	l_isToggle = false;
-	for (i = 0; i < 5; i++) {
-		j = l_isToggle ? l_u8XCenter - l_u8LongLineHalfLenght
-		               : l_u8XCenter - l_u8ShortLineHalfLenght;
-		k = l_isToggle ? l_u8XCenter + l_u8LongLineHalfLenght
-		               : l_u8XCenter + l_u8ShortLineHalfLenght;
-
-		this->drawLine(j, l_u8YStart + l_u8YSeparation * i, k,
-		               l_u8YStart + l_u8YSeparation * i);
-		l_isToggle = !l_isToggle;
-	}
-	/**
-	 * Circulo centrado en (h, k) de radio r:
-	 *
-	 * (x-h)^2+(y-k)^2=r^2
-	 *
-	 * Para x y y se tiene:
-	 *
-	 * x = +- sqrt(r^2 - (y-k)^2) + h
-	 * y = +- sqrt(r^2 - (x-h)^2) + k
-	 *
-	 * a(x,y) = tan((y-k) / (x-h))
-	 *
-	 * a(y) = tan((y-k) / (+- sqrt(r^2 - (y-k)^2)))
-	 * a(x) = tan((+- sqrt(r^2 - (x-h)^2)) / (x-h))
-	 *
-	 * y(a, x) = (x-h)*atan(a) + k
-	 * x(a, y) = (y-k)/atan(a) + h
-	 *
-	 */
-
-	Graphics_setForegroundColor(&task::Accelerometer::m_stStaticContext,
-	                            GRAPHICS_COLOR_WHITE);
-
-	this->drawLine(114, 38, 119, 35);
-	this->drawLine(114, 37, 119, 34);
-
-	this->drawLine(14, 38, 9, 35);
-	this->drawLine(14, 37, 9, 34);
-
-	this->drawLine(35, 16, 32, 11);
-
-	this->drawLine(93, 16, 96, 11);
 }
 
 void task::Accelerometer::printResults() {
