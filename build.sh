30303704049f887ef717a9b901417acd140edcd5
Jeancarlo Hidalgo <jeancahu@gmail.com>
2019-05-19 19:35:43 -0600

    Update build script refresh Debug temp files and makefile

--- build.sh
+++ build.sh
@@ -9,6 +9,16 @@ declare PROJ_DEBUG_PATH=./Debug
 declare PROJ_TARGCNF=./targetConfigs/MSP432P401R.ccxml
 declare PROJ_TARGOUT
 
+## IDE VARS DECLARATION
+declare ECL_IDE=ccs/eclipse/eclipse
+declare ECL_IDE_FLAGS='-noSplash -data $ECL_WS
+        -application com.ti.ccstudio.apps.projectBuild
+	-ccs.projects $PROJ_NAME
+	-ccs.configuration Debug'
+declare PROJ_NAME="$( grep name .project | head -n 1 |
+			 tr '<>' '|' | cut -f 3 -d'|' )"
+declare ECL_WS=$HOME/workspace_v9/
+
 for CCS_DIR in $HOME/ti/ccs90{0,1} /opt/ti/ccs90{0,1}
 do
     test -d $CCS_DIR || continue
@@ -17,7 +27,8 @@ done
 
 if [ $CCS_PATH ]
 then
-    ( cd $PROJ_DEBUG_PATH && $CCS_PATH/$CCS_MAKE_UTIL $CCS_MAKE_UTIL_FLAGS )
+    ( cd $PROJ_DEBUG_PATH && $CCS_PATH/$CCS_MAKE_UTIL $CCS_MAKE_UTIL_FLAGS ) ||
+	( rm -rf $PROJ_DEBUG_PATH ; eval $CCS_PATH/$ECL_IDE $ECL_IDE_FLAGS )
     for OUT_FILE in $PROJ_DEBUG_PATH/*.out
     do
 	[ $PROJ_TARGOUT ] && {
@@ -25,7 +36,7 @@ then
 	    exit 2 ; }			   
 	PROJ_TARGOUT=$OUT_FILE
     done
-    lsusb -d $TI_CARD_ID &&
+    lsusb -d $TI_CARD_ID && test -f $PROJ_TARGOUT &&
 	$CCS_PATH/$CCS_LOAD_UTIL -c $PROJ_TARGCNF -l $PROJ_TARGOUT
 else
     printf 'Error: CCS_PATH not found.\n' &>/dev/stderr
