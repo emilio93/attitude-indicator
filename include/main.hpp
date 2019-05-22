1d8aa0315c4a88934574f46201225cf988e463a9
Jeancarlo Hidalgo <jeancahu@gmail.com>
2019-05-21 22:25:23 -0600

    Revert "Update context RollIndicator"
    This reverts commit e20b9fef313aad0433529cad5fa8b55ec0a33791.

--- include/main.hpp
+++ include/main.hpp
@@ -4,7 +4,7 @@
 // ~1ms with 48MHz clock.
 // 48e6ticks/seg * 1e-3 seg = 48e3ticks
 // The main Setup function for the application
-#define TIMER32_COUNT 0x48e3
+#define TIMER32_COUNT 48e3
 
 void Setup(void);
 void debugSetup(void);
